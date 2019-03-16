4e2975dc3a8e989f7e729612ee2c3eb5d026476b
tetrohed <armin65@gmail.com>
2019-03-16 20:26:08 +0100

    add first functional test

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,20 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
 
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+
+        inputbox.send_keys('buy a tdd book and learn about django')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep('1')
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: buy a tdd book and learn about django' for row in rows))
+
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,6 @@ from lists.views import home_page
 from django.http import HttpRequest
 import unittest
 
-
 class HomePageTest(TestCase):
     
     def test_root_url_resolves_to_home_page_view(self):
