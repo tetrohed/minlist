cff508c31f44ae53dafcb7104100883eef435d2c
tetrohed <armin65@gmail.com>
2019-03-17 18:08:27 +0100

    add missing templates add second list item retrieval to functional tests add item model

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000/lists')
         self.assertIn('To-Do', self.browser.title)
@@ -25,9 +30,13 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: buy a tdd book and learn about django', [row.text for row in rows])
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('just try javascript and you will learn it')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: buy a tdd book and learn about django')
+        self.check_for_row_in_list_table('2: just try javascript and you will learn it')
 
 
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 from lists.views import home_page
+from lists.models import Item
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 import unittest
@@ -19,3 +20,22 @@ class HomePageTest(TestCase):
         response = self.client.post(reverse('lists:home'), data={'item_text': 'A new list item'})
         self.assertTemplateUsed(response, 'lists/home.html')
         self.assertIn('A new list item', response.content.decode())
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) item in list'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item in list'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) item in list')
+        self.assertEqual(second_saved_item.text, 'The second item in list')
