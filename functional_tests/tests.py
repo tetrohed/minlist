cb711e5775acd8bdfa33a14188ba10202f7da750
tetrohed <armin65@gmail.com>
2019-03-19 19:42:43 +0100

    remove time sleeps in functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
@@ -8,6 +9,7 @@ import unittest
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
+        self.MAX_WAIT = 10
         self.browser = webdriver.Chrome()
 
     def tearDown(self):
@@ -17,6 +19,19 @@ class NewVisitorTest(LiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return 
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise e
+                time.sleep(.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url + '/lists')
@@ -29,12 +44,12 @@ class NewVisitorTest(LiveServerTestCase):
 
         input_box.send_keys('buy a tdd book and learn about django')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table('1: buy a tdd book and learn about django')
 
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('just try javascript and you will learn it')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table('2: just try javascript and you will learn it')
 
         self.check_for_row_in_list_table('1: buy a tdd book and learn about django')
         self.check_for_row_in_list_table('2: just try javascript and you will learn it')
