2e8136185a59ccd2dfca07b821cfbd8e7001b03c
tetrohed <armin65@gmail.com>
2019-03-19 19:33:14 +0100

    fix test to use test server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import time
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -18,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000/lists')
+        self.browser.get(self.live_server_url + '/lists')
         self.assertIn('To-Do', self.browser.title)
 
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -38,6 +39,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('1: buy a tdd book and learn about django')
         self.check_for_row_in_list_table('2: just try javascript and you will learn it')
 
-
-if __name__ == '__main__':
-    unittest.main()
