c3454ead593291cce6687eb18950d243830225d1
tetrohed <armin65@gmail.com>
2019-03-20 20:52:01 +0100

    fix integration tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -70,17 +70,17 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.browser.get(self.live_server_url + '/lists')
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn(page_text, 'call amy')
+        self.assertNotIn('call amy', page_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('buy milk')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('buy milk')
+        self.wait_for_row_in_list_table('1: buy milk')
 
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, 'lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn(page_text, 'call amy')
-        self.assertIn(page_text, 'buy milk')
+        self.assertNotIn('call amy', page_text)
+        self.assertIn('buy milk', page_text)
