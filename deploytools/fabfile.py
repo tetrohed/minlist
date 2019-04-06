ca77a199e6dc37075aa3d45aca87b02d6f2e33ba
tetrohed <armin65@gmail.com>
2019-04-06 16:38:36 +0200

    add site name to allowed hosts

--- deploytools/fabfile.py
+++ deploytools/fabfile.py
@@ -3,17 +3,20 @@ import subprocess
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
-def deploy():
-    completedProcess = subprocess.run(["git", "remote", "get-url", "origin"], capture_output=True)
+
+def deploy(debug='false'):
+    completedProcess = subprocess.run(
+        ["git", "remote", "get-url", "origin"], capture_output=True)
     repo_url = completedProcess.stdout.strip()
     site_folder = f'/home/{env.user}/websites/{env.host}'
     run(f'mkdir -p {site_folder}')
     with cd(site_folder):
         _get_latest_source(repo_url.decode("utf-8"))
         _update_virtualenv()
-        _create_or_update_dotenv()
+        _create_dotenv(debug)
         _update_static_files()
         _update_database()
+
 
 def _get_latest_source(repo_url):
     if exists('.git'):
@@ -23,21 +26,28 @@ def _get_latest_source(repo_url):
 
     run(f'git pull origin')
 
+
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
         run(f'python3.6 -m venv virtualenv')
         run('./virtualenv/bin/pip install -r requirements.txt')
 
-def _create_or_update_dotenv():
-    append('.env', 'DJANGO_DEBUG_FALSE=y')
+
+def _create_dotenv(debug):
+    run('rm .env')
+    if (debug == 'false'):
+        append('.env', 'DJANGO_DEBUG_FALSE=y')
     append('.env', f'SITENAME={env.host}')
     current_contents = run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
-        new_secret = ''.join(random.SystemRandom().choices('abcdefghlmnopqrstuvwxyz0123456789', k=50))
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghlmnopqrstuvwxyz0123456789', k=50))
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+
 
 def _update_static_files():
     run('./virtualenv/bin/python manage.py collectstatic --noinput')
+
 
 def _update_database():
     run('./virtualenv/bin/python manage.py migrate --noinput')
--- minlist/settings.py
+++ minlist/settings.py
@@ -18,12 +18,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
     DEBUG = True
     SECRET_KEY = 'dev_insecure_ckey'
-    ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = [os.environ['SITENAME']]
 # Application definition
 
 INSTALLED_APPS = [
